6ecaacff434bc1fa07097adfe2ee3b7e52bd82f6
HDK <hdk8572@gmail.com>
2023-06-07 16:56:18 +0900

    Subject_Music
    복습 무조건 해야한다 _ 16:56

--- Music/src/com/kh/practice/list/music/controller/MusicController.java
+++ Music/src/com/kh/practice/list/music/controller/MusicController.java
@@ -1,5 +1,49 @@
 package com.kh.practice.list.music.controller;
 
-public class MusicController {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.kh.practice.list.music.model.vo.Music;
+
+public class MusicController {
+	private List<Music>	list = new ArrayList<Music>();
+	
+	public int addList(Music music) {
+		int result = 0;	// 0:추가실패, 1:추가성공
+		// TODO
+		return result;
+	}
+	public int addAtZero(Music music) {
+		int result = 0; // 0:추가실패, 1:추가성공
+		// TODO
+		return result;
+	}
+	public List<Music> printAll(){
+		// TODO
+		return list;
+	}
+	public Music searchMusic(String title) {
+		Music result = null;
+		// TODO
+		return result;
+	}
+	public Music removeMusic(String title) {
+		// TODO
+		return null;
+	}
+	public Music setMusic(String title, Music music) {
+		// TODO
+		return null;
+	}
+	public int ascTitle() {
+		int result = 0;
+		// TODO
+		return result;
+	}
+	public int descSinger() {
+		int result = 0;
+		// TODO
+		return result;
+	}
+	
 }
--- Music/src/com/kh/practice/list/music/model/compare/AscTitle.java
+++ Music/src/com/kh/practice/list/music/model/compare/AscTitle.java
@@ -2,4 +2,9 @@ package com.kh.practice.list.music.model.compare;
 
 public class AscTitle {
 
+	public int compare(Object o1, Object o2) {
+		int result = 0;
+		// TODO
+		return result;
+	}
 }
